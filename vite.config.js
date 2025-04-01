bcc2f449c15516255886b47911309eb347f78c3c
Valentina Pelaez Echeverri <117761077+valenpelaez99@users.noreply.github.com>
2025-03-31 23:53:38 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const backendUrl = process.env.VITE_API_URL || 'http://localhost:8080';
+const backendUrl = 'http://34.204.69.190:8080';
 //const backendUrl = 'http://localhost:8080';
 
 export default defineConfig({
