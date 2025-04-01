878f12f9883b4a53ffc741ebed2351f763ba7c87
Erick Olivares <erickolivareszea@gmail.com>
2025-03-31 21:35:32 -0500

    cambio backend URL

--- src/Services/apiConfig.js
+++ src/Services/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://52.90.61.225";
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
 const api = axios.create({
   baseURL: API_URL,
